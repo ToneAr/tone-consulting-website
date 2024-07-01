3d90aba18874a1727018b89e69b1a5b459543a48
Tone <tonya@wolfram.com>
2024-07-01 23:01:14 +0100

    Text changes

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -107,7 +107,7 @@ function Home() {
 
           <DisplayPanel>
               <Typography variant='h1' color={theme.palette.warning.main} textAlign={'center'}>
-                <WarningIcon sx={{fontSize: 150}}/><br/>This site is still under construction
+                <WarningIcon sx={{fontSize: 150}}/><br/>This site is under construction
               </Typography>
           </DisplayPanel>
 
